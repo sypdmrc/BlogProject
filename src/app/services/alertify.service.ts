10e20d00a2265e30fd6e960120e1bdd143f65f19
Şuayip DEMİRCİ <suayip.demirci53@gmail.com>
2022-04-04 15:32:51 +0300

    alertify position

--- src/app/services/alertify.service.ts
+++ src/app/services/alertify.service.ts
@@ -8,14 +8,17 @@ export class AlertifyService{
   constructor() {}
 
   success(message:string){
+    alertify.set('notifier','position', 'bottom-left');
     alertify.success(message);
   }
 
   error(message:string){
+    alertify.set('notifier','position', 'bottom-left');
     alertify.error(message);
   }
 
   warning(message:string){
+    alertify.set('notifier','position', 'bottom-left');
     alertify.warning(message);
   }
 }
