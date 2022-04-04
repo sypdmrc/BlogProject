51ffd129c35971278d24cd007118f1ef32bf8757
Şuayip DEMİRCİ <suayip.demirci53@gmail.com>
2022-04-04 15:57:34 +0300

    dataların json.server'a aktarılması

--- src/app/models/category.repository.ts
+++ src/app/models/category.repository.ts
@@ -6,13 +6,7 @@ export class CategoryRepository{
   constructor(){
 
     this.categories=[
-      {id:1,name:"HTML"},
-      {id:2,name:"CSS"},
-      {id:3,name:"BOOTSTRAP"},
-      {id:4,name:"JAVASCRIPT"},
-      {id:5,name:"JQUERY"},
-      {id:6,name:"REACT"},
-      {id:7,name:"ANGULAR"},
+
     ]
   }
 
