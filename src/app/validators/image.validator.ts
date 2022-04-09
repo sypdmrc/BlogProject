2c067437d4885d10f49a6af4fb96c0a87db9b50a
Şuayip DEMİRCİ <suayip.demirci53@gmail.com>
2022-04-10 00:24:22 +0300

    deneme

--- src/app/validators/image.validator.ts
+++ src/app/validators/image.validator.ts
@@ -1,17 +1,22 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 
+
+
 export class ImageValidator {
 
   static isValidExtension(control: AbstractControl): ValidationErrors | null {
     const value = control.value as string;
 
-    if (value?.endsWith(".jpeg") || value?.endsWith(".jpg") || value?.endsWith(".png")){
-      return null;
-    }
-    else{
-      return{
-        wrongExtension:true
+
+      if (value?.endsWith(".jpeg") || value?.endsWith(".jpg") || value?.endsWith(".png")){
+        return null;
       }
-    }
+      else{
+        return{
+          wrongExtension:true
+        }
+      }
+
+
   }
 }
